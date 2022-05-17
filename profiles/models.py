abb67e5836e45802ff447d7ee5f68905a594e6ab
RAVIKIRANRAOJ <ravikiran.rao@avetoconsulting.com>
2022-05-17 20:13:53 +0530

    model design is done.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    date_of_birth = models.DateField(null=True) # format will be YYYY-MM-DD
+    class_year = models.PositiveIntegerField(null=True)
+    email_field = models.EmailField()
